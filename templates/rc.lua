8a70aac125904cb76198a6d4b22cf0fa6ac57047
Siorde <quentin.aubry94@gmail.com>
2018-10-12 20:28:29 +0100

    Personnalisation du theme et du wallpaper

--- templates/rc.lua
+++ templates/rc.lua
@@ -41,7 +41,7 @@ end
 
 -- [[[ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init(awful.util.get_themes_dir() .. "default/theme-{{user}}.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
